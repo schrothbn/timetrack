35f7a3ac0308f6ddf1044f1d2a63bfd5a8a56c72
Benjamin Schroth <info@benjamin-schroth.dev>
2022-01-23 11:28:40 +0100

    store: remove unused code

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,16 @@
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 const state = reactive({
-    user: null,
-    projects: [],
-    tracking: false,
+  user: null,
 });
 
 const methods = {
-    setUser(payload) {
-        state.user = payload ? payload.user : null 
-    },
-    setProjects(projects) {
-        state.projects = projects 
-    },
-    addProject(project) {
-        state.projects.push(project)
-    },
-    removeProject(project) {
-        const idx = state.projects.findIndex(p => p === project);
-        state.projects.slice(idx, 1);
-    },
-    startTracking() {
-        state.tracking = true;
-    },
-    stopTrakcing() {
-        state.tracking = false;
-    }
-}
+  setUser(payload) {
+    state.user = payload ? payload.user : null;
+  },
+};
 
 export default {
-    state,
-    methods
+  state,
+  methods,
 };
